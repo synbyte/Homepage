429d38ad5847e9fc7a669e5a1ae79fd7703e9d4a
synbyte <81550106+synbyte@users.noreply.github.com>
2023-03-29 12:58:38 -0700

    2 files modified, 1 added

---
+++ src/lib/Search.svelte
@@ -0,0 +1,18 @@
+<form onSubmit="javascript:goS();">
+<input id='q' type="text">
+<button type="submit">Go</button>
+</form>
+<script>
+const q = document.getElementById("q").value
+function goS() {
+  window.open("https://google.com/search?="+q);
+}
+</script>
+<style>
+form {
+  width:100%;
+}
+input {
+  
+}
+</style>
